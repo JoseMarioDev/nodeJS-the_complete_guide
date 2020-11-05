72baa9d5e79e95baf8b8916f85ecf139f22a914e
JoseMarioDev <josemariodev@gmail.com>
2020-11-05 01:00:14 -0600

    :heavy_plus_sign: Install body parser

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require('express'); // installs express
 const app = express(); //sets app to use express. sometimes called server?
+const bodyParser = require('body-parser');
 
-//ex of middleware
-app.use((req, res, next) => {
-  console.log('in middleware');
-  next(); // use next() to move to next middleware below
+app.use(bodyParser.urlencoded({ extended: false })); //parse body
+
+app.use('/', (req, res, next) => {
+  console.log('this always runs');
+  next();
 });
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+  console.log('middleware');
+  res.send('<h2>add product page</h2>');
+});
+app.use('/', (req, res, next) => {
   console.log('another middleware');
   res.send('<h2>hello from express</h2>');
 });
