edd8e6d600f8e46dac71b57641fbc39518f08c70
JoseMarioDev <josemariodev@gmail.com>
2020-11-05 00:30:55 -0600

    :recycle: Add comments app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // installs express
+const app = express(); //sets app to use express. sometimes called server?
 
+//ex of middleware
 app.use((req, res, next) => {
   console.log('in middleware');
   next(); // use next() to move to next middleware below
@@ -10,4 +11,5 @@ app.use((req, res, next) => {
   res.send('<h2>hello from express</h2>');
 });
 
+// listen on port 3000.  can refactor to use .env like in class eventually
 app.listen(3000);
