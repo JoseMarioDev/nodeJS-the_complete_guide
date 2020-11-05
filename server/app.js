935bca5729e140a7c4bff03e8ad5b06a5a7be62e
JoseMarioDev <josemariodev@gmail.com>
2020-11-05 00:03:48 -0600

    :white_check_mark: testing middleware setup

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) => {
+  console.log('in middlewear');
+});
 
 const server = http.createServer(app);
 server.listen(3000);
