416338f9bb3280881a2363627ee817191b081e97
JoseMarioDev <josemariodev@gmail.com>
2020-11-05 00:25:39 -0600

    :white_check_mark: through 62
    refactor app.js. no need for 'http'

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 const app = express();
 
@@ -12,5 +10,4 @@ app.use((req, res, next) => {
   res.send('<h2>hello from express</h2>');
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
