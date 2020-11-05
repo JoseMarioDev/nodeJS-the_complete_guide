1f2b5ccefa5c176406c52a1c3b11e9c92deb1d77
josemariodev <josemariodev@gmail.com>
2020-11-05 09:13:20 -0600

    :art: Add routes
    shop and admin in routes dir

--- server/app.js
+++ server/app.js
@@ -2,21 +2,15 @@ const express = require('express'); // installs express
 const app = express(); //sets app to use express. sometimes called server?
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false })); //parse body
 
-app.use('/', (req, res, next) => {
-  console.log('this always runs');
-  next();
-});
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.use('/add-product', (req, res, next) => {
-  console.log('middleware');
-  res.send('<h2>add product page</h2>');
-});
-app.use('/', (req, res, next) => {
-  console.log('another middleware');
-  res.send('<h2>hello from express</h2>');
-});
+
 
 // listen on port 3000.  can refactor to use .env like in class eventually
 app.listen(3000);
