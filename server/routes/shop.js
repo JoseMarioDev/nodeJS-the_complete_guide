1b3e3bb43ef14a40de7be98dc00e09c8744e78ac
JoseMarioDev <josemariodev@gmail.com>
2020-11-06 16:34:08 -0600

    :sparkles: Serving HTML pages
    shop and admin

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 router.get('/', (req, res, next) => {
-res.send('<h2>hello from express</h2>')
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
-module.exports = router
+module.exports = router;
