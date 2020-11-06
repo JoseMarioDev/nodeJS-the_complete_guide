93f3c9bbbb1130b20cd24f4694bd9c8449b3a1d2
JoseMarioDev <josemariodev@gmail.com>
2020-11-06 15:48:33 -0600

    :goal_net: Adding 404 route

--- server/app.js
+++ server/app.js
@@ -2,15 +2,18 @@ const express = require('express'); // installs express
 const app = express(); //sets app to use express. sometimes called server?
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false })); //parse body
 
-app.use(adminRoutes)
-app.use(shopRoutes)
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-
+// 404 middleware to catch all
+app.use((req, res, next) => {
+  res.status(404).send('<h2>Page not found</h2>');
+});
 
 // listen on port 3000.  can refactor to use .env like in class eventually
 app.listen(3000);
