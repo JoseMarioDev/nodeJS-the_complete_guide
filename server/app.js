81040013e0604fd2e23bfae42098926941a81fe0
JoseMarioDev <josemariodev@gmail.com>
2020-11-10 00:45:51 -0600

    :sparkles: Adding controllers

--- server/app.js
+++ server/app.js
@@ -3,21 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-app.set('view engine', 'pug'); // let express know that pug is the template engine we're using
-app.set('views', 'views'); //tells express where our templates are stored.  in views folder
+const errorController = require('./controllers/error');
 
-const adminData = require('./routes/admin');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404');
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- server/routes/shop.js
+++ server/routes/shop.js
@@ -2,14 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
-const adminData = require('./admin');
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', { prods: products, docTitle: 'Shop' }); //basically passing products obj as props to shop.pug
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
