82dade9fe1c907e89a4ce9bef1d7b2f4c6387876
JoseMarioDev <josemariodev@gmail.com>
2020-11-09 19:59:27 -0600

    :zap: Rendering dynamic content in Shop

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -8,7 +8,8 @@ const router = express.Router();
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' }); //basically passing products obj as props to shop.pug
 });
 
 module.exports = router;
